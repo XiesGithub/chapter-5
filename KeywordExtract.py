3401ce36b8f66a121e80ba4e07e234af17e9179b
Xie <13307000056@163.com>
2023-01-19 16:27:29 +0800

    second commit

--- KeywordExtract.py
+++ KeywordExtract.py
@@ -14,7 +14,7 @@ def get_stopword_list():
     # 停用词表存储路径，每一行为一个词，按行读取进行加载
     # 进行编码转换确保匹配准确率
     stop_word_path = './stopword.txt'
-    stopword_list = [sw.replace('\n', '') for sw in open(stop_word_path,encoding='utf-8').readlines()]
+    stopword_list = [sw.replace('\n', '') for sw in open(stop_word_path,encoding='utf-8').readlines()]#停用词列表
     return stopword_list
 
 
